d499542ee0bbb9e68f70d32b9233b743b95c3e2d
V1ane24 <cv25001@esfe.agape.edu.sv>
2026-07-31 10:36:07 -0600

    Ejercicio 11

---
+++ scripts/ejercicio11.js
@@ -0,0 +1,14 @@
+let color = "rojo";
+
+switch (color) {
+    case "rojo":
+    alert("Detenerse");
+    break;
+
+    case "verde":
+        alert("Avanzar");
+        break;
+
+        default:
+            alert("Precaución");
+}
